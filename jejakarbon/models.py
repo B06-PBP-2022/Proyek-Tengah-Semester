ccb92de6e4ead62e233a57016772acb57c6f24b9
griseldans <griseldaneysasaadiya@gmail.com>
2022-10-22 15:18:30 +0700

    create model for carbon calculator

--- jejakarbon/models.py
+++ jejakarbon/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class User(models.Model):
+    # nitip field user sm totalCarbon yah -neysa
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_carbon = models.PositiveIntegerField(default=0)
+
+class DataCarbon(models.Model):
+    jenis_pemakaian = models.CharField(max_length=10)
+    jumlah_kwh = models.FloatField(default=0)
+    jumlah_km = models.FloatField(default=0)
+    jumlah_bensin_liter = models.IntegerField(default=0)
+    jenis_bbm = models.CharField(max_length=6)
