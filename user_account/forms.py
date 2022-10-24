2a693f81f23839ddabc67087201e8a85ce4c307d
daffarestupratama <daffa.ilham@ui.ac.id>
2022-10-24 21:41:04 +0700

    Add change_username in user account views.py

--- user_account/forms.py
+++ user_account/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
-class EditProfileForm(forms.Form):
+class EditUsernameForm(forms.Form):
     username = forms.CharField(max_length=200)
+
+class EditPasswordForm(forms.Form):
+    old_username = forms.CharField(max_length=200)
     password = forms.CharField(max_length=200)
+    confirm_password = forms.CharField(max_length=200)
--- user_account/views.py
+++ user_account/views.py
@@ -8,15 +8,31 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect,HttpResponse
 
+from user_account.forms import EditPasswordForm, EditUsernameForm
+
 # @login_required(login_url='/login/')
 def show_account(request):
     user = User.objects.get(user=request.user)
     data_carbon = DataCarbon.objects.filter(user=user)
+    username_form = EditUsernameForm()
+    password_form = EditPasswordForm()
     context = {
         'user': user,
         'data_carbon': data_carbon,
+        'username_form': username_form,
+        'password_form': password_form
     }
     return render(request, 'user_account.html', context)
+
+def change_username(request):
+    if request.method == 'POST':
+        user = User.objects.get(username = request.user.username)
+        user.username = request.POST.get('username')
+        user.save()
+        messages.success(request, 'Your username was successfully updated!')
+    else:
+        messages.error(request, 'Please correct the error below.')
+    return HttpResponse('')
 
 def change_password(request):
     if request.method == 'POST':
@@ -28,6 +44,4 @@ def change_password(request):
             return redirect('change_password')
         else:
             messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
     return HttpResponse('')
