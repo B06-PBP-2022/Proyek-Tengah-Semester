5d4bdbda3f9d4dcddff83ba7aa0f0b4d7359b3c7
griseldans <griseldaneysasaadiya@gmail.com>
2022-10-25 12:08:05 +0700

    hapus folder

--- jejakarbon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from views import carbon_calculator
-
-app_name = ['jejakarbon']
-
-urlpatterns = [
-    path('calculate-carbon/', carbon_calculator, name='carbon_calculator'),
-]
-
