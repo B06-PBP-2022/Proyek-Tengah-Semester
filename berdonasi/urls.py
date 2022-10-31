0ec16eb21859acfae0e21654e37f89b2729b6426
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2022-11-01 03:29:15 +0700

    membuat pembayaran.html

--- berdonasi/urls.py
+++ berdonasi/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from berdonasi.views import show_masukkan_nominal
-from berdonasi.views import test
+from berdonasi.views import pembayaran
 
 
 app_name = 'berdonasi'
 
 urlpatterns = [
     path('',show_masukkan_nominal, name='show_masukkan_nominal'),
-    path('test',test, name='test')
+    path('pembayaran',pembayaran, name='pembayaran')
 ]
