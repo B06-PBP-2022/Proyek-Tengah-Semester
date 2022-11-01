afe5f3f8ca185c5e4e8bbe65c20bcad155e6b383
raniaarn <rania.maharani11@ui.ac.id>
2022-11-01 18:01:28 +0700

    add name required to user

--- faq/models.py
+++ faq/models.py
@@ -6,4 +6,4 @@ class Faq(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
     question = models.CharField(max_length=250,blank=False)
     answer = models.TextField(blank=True)
-    username = models.CharField(blank=True, max_length=250)
+    username = models.CharField(max_length=250)
