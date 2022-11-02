ce153aea17b446a628f1d3eba3435415bfa8536a
raniaarn <rania.maharani11@ui.ac.id>
2022-11-02 21:25:49 +0700

    home style update

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -7,13 +7,13 @@ $(document).ready(() => {
               <div class="col sec-card">
                 <div class="card mb-3">
                     <img class="card-img" src="static/image/donating_card.png" class="img-fluid rounded-start">
-                    <div class="card-body">
+                    <div class="card-body d-flex flex-column">
                     <h5 class="text card-title">${daftar.fields.tema_kegiatan}</h5>
                     <p class="text card-text">Dari: ${daftar.fields.pencetus_donasi}</p>
                     <span style="color: #75C270; background-color: white;" class="badge text-wrap text-truncate"> ${daftar.fields.total_donasi_terkumpul} / ${daftar.fields.target_donasi}</span>
                     <hr class="divider-card">
                     <p class="text card-text">${daftar.fields.deskripsi}</p>
-                    <a href="donasi/${daftar.pk}/" class="btn-white sbmit">Donasi</a>
+                    <a href="donasi/${daftar.pk}/" class="mt-auto btn-white sbmit">Donasi</a>
                     </div>
                 </div>
             </div>
