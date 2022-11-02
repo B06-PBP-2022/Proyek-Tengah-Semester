9235d45515ebb7e653ec412a599dd279683a3861
griseldans <griseldaneysasaadiya@gmail.com>
2022-11-02 22:34:08 +0700

    styling (masih ga bagus)

--- kalkulator/urls.py
+++ kalkulator/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from kalkulator.views import show_kalkulator, add_carbon_kendaraan, add_carbon_listrik
+from kalkulator.views import show_json_carbon_detail, user_histori
 
 app_name = 'kalkulator'
 
 urlpatterns = [
     path('', show_kalkulator, name='show_kalkulator'),
     path('calculate-kendaraan/', add_carbon_kendaraan, name='calculate-kendaraan'),
-    path('calculate-listrik/', add_carbon_listrik, name='calculate-listrik')
+    path('calculate-listrik/', add_carbon_listrik, name='calculate-listrik'),
+    path('kalkulator-json', show_json_carbon_detail, name='show_json_carbon_detail'),
+    path('get_total_carbon/', user_histori, name='user_histori'),
 ]
