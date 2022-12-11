a75030dfed59a8efab9aae03038455735601444c
daffarestupratama <daffa.ilham@ui.ac.id>
2022-12-12 01:24:29 +0700

    change jsonresponse to httpresponse

--- user_profile/views.py
+++ user_profile/views.py
@@ -86,9 +86,9 @@ def username_available(request):
     username = request.GET.get('username')
     user = User.objects.filter(username = username).exists()
     if user:
-        return JsonResponse(False)
+        return HttpResponse(False)
     else:
-        return JsonResponse(True)
+        return HttpResponse(True)
 
 @login_required(login_url='/login/')
 def username_json(request):
